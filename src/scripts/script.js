877ecfb4562eae0bc931c32f72a469c3836ba591
Nikita Mateyuk <work.acr0matic@gmail.com>
2019-12-19 20:37:53 +0300

    Сверстан слайдер в секции "Мастера"
    Добавил фон во вторую секцию
    Надо доделать иконки у слайдера

---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper(".swiper-container", {
+    direction: "vertical",
+    loop: true
+  });
+});
+
+console.log("12312");
