fbf91a7c1577024c450c4b81f206b06c5c90cb8c
Nikita Mateyuk <work.acr0matic@gmail.com>
2019-12-22 19:26:06 +0300

    Доделана стилизация и исправлены некоторые ошибки

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,10 +5,17 @@ window.addEventListener("DOMContentLoaded", () => {
     centeredSlides: true,
     slideActiveClass: "active",
     slidesPerView: 2,
+    spaceBetween: 120,
     navigation: {
-      nextEl: '.swiper-button-next-1',
-      prevEl: '.swiper-button-prev-1',
+      nextEl: ".swiper-button-next-1",
+      prevEl: ".swiper-button-prev-1"
     },
+
+    breakpoints: {
+      1445: {
+        spaceBetween: 0
+      }
+    }
   });
 
   var SecondSwiper = new Swiper(".swiper2", {
@@ -17,9 +24,23 @@ window.addEventListener("DOMContentLoaded", () => {
     slideActiveClass: "active",
     slidesPerView: 1,
     navigation: {
-      nextEl: '.swiper-button-next-2',
-      prevEl: '.swiper-button-prev-2',
-    },
+      nextEl: ".swiper-button-next-2",
+      prevEl: ".swiper-button-prev-2"
+    }
   });
-});
 
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute("href").substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    });
+  }
+});
